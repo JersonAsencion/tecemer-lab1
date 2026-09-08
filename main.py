4a40cf05a83854fce0058fc48e00291f7df4a064
Jerson <e_2025100767M@uncp.edu.pe>
2026-09-08 09:28:05 -0500

    chore: configura entorno virtual y primner script funcional

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import requests
-try:
-    respuesta=requests.get("https://official-joke-api.appspot.com/random_joke")
-    respuesta.raise_for_status()
-    datos=respuesta.json()
-    print(datos["setup"]) 
-    print(datos["punchline"])   
-except requests.RequestException as error:
-    print(f"No se pudo obtener el chiste: {error}")
+respuesta=requests.get("https://official-joke-api.appspot.com/random_joke")
+datos=respuesta.json()
+print(datos["setup"]) 
+print(datos["punchline"])   
